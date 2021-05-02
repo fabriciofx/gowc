eb07fa0f3169942213afa325136b41fbe8c11d21
Fabrício Cabral <fabriciofx@gmail.com>
2021-05-01 21:40:22 -0300

    First version which works

--- gowc.go
+++ gowc.go
@@ -8,7 +8,7 @@ import (
 	"path/filepath"
 )
 
-func visit(files *[]string) filepath.WalkFunc {
+func fromDir(files *[]string) filepath.WalkFunc {
 	return func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			log.Fatal(err)
@@ -20,40 +20,39 @@ func visit(files *[]string) filepath.WalkFunc {
 	}
 }
 
-func countLines(pipe chan int, file string) {
-	content, err := os.ReadFile(file)
+func filenames(path string) []string {
+	var filenames []string
+	err := filepath.Walk(path, fromDir(&filenames))
 	if err != nil {
 		panic(err)
 	}
-	count := bytes.Count(content, []byte{'\n'})
-	fmt.Printf("Lines in '%s': %d\n", file, count)
-	pipe <- count
+	return filenames
 }
 
-func sumLines(pipe chan int) int {
+func countLines(filenames []string) int {
+	pipe := make(chan int)
+	for _, filename := range filenames {
+		go func(f string) {
+			content, err := os.ReadFile(f)
+			if err != nil {
+				panic(err)
+			}
+			count := bytes.Count(content, []byte{'\n'})
+			fmt.Printf("Lines in '%s': %d\n", f, count)
+			pipe <- count
+		}(filename)
+	}
 	sum := 0
-	for {
+	for range filenames {
 		count, ok := <-pipe
 		if ok {
 			sum = sum + count
-		} else {
-			break
 		}
 	}
 	return sum
 }
 
 func main() {
-	pipe := make(chan int, 200)
-	var files []string
-	root := "dataset"
-	err := filepath.Walk(root, visit(&files))
-	if err != nil {
-		panic(err)
-	}
-	for _, file := range files {
-		go countLines(pipe, file)
-	}
-	sum := sumLines(pipe)
-	fmt.Printf("Total: %d\n", sum)
+	filenames := filenames("dataset")
+	fmt.Printf("Total: %d\n", countLines(filenames))
 }
